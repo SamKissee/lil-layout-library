99fc42ae599801645c52cc983a12e8d15f7bdfc0
Ginger Lee Kretschmer <kretschmons@gmail.com>
2017-12-20 11:03:08 -0800

    add fade-in animations to homepage

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,33 @@ $(document).ready(function() {
    {selector: '.parallax2', offset: 200, callback: function(el) {
          Materialize.fadeInImage($(el));
        } },
+   {selector: '.gallery-header', offset: 200, callback: function(el) {
+         Materialize.fadeInImage($(el));
+       } },
+   {selector: '.img1', offset: 300, callback: function(el) {
+         Materialize.fadeInImage($(el));
+       } },
+   {selector: '.img2', offset: 300, callback: function(el) {
+         Materialize.fadeInImage($(el));
+       } },
+   {selector: '.img3', offset: 300, callback: function(el) {
+         Materialize.fadeInImage($(el));
+       } },
+   {selector: '.img4', offset: 300, callback: function(el) {
+         Materialize.fadeInImage($(el));
+       } },
+   {selector: '.img5', offset: 300, callback: function(el) {
+         Materialize.fadeInImage($(el));
+       } },
+   {selector: '.img6', offset: 300, callback: function(el) {
+         Materialize.fadeInImage($(el));
+       } },
+   {selector: '.parallax3', offset: 200, callback: function(el) {
+         Materialize.fadeInImage($(el));
+       } },
+   {selector: '.page-footer', offset: 200, callback: function(el) {
+         Materialize.fadeInImage($(el));
+       } },
  ];
  Materialize.scrollFire(options);
 
